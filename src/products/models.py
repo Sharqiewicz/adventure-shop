aea6987e101c118e2f77645e9903b51ad8d9b122
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-06-12 19:12:45 +0200

    add products to the admin panel

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    opinion = models.TextField()
