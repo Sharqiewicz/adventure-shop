a472f3d50bee183e59cf51ac6cd0a05525f957cd
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-06-12 20:16:47 +0200

    add templates

--- src/adventureshop/urls.py
+++ src/adventureshop/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from products.views import homepage_view
+
 urlpatterns = [
+    path('', homepage_view, name="home"),
     path('admin/', admin.site.urls),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def homepage_view(request):
+    return render(request, 'index.html', {})
