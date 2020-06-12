29dee034d183fa03b74b1ce6a8802dc8fbfc22be
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-06-12 19:48:35 +0200

    add min and max value to the product opinion

--- src/products/models.py
+++ src/products/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Product(models.Model):
     title = models.TextField()
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
-    opinion = models.TextField()
+    opinion = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(0)])
