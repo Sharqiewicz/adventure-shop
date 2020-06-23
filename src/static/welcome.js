0073f05878b32e609697225987d4d5149a643736
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-06-23 17:26:53 +0200

    add text animation in welcome section

--- src/static/welcome.js
+++ src/static/welcome.js
@@ -1,5 +1,8 @@
 const welcome__header = document.querySelector('.welcome__header');
-const all = [welcome__header]
+const adv__buttons = document.querySelectorAll('.adv__button');
+const socials = document.querySelector('.socials__list');
+const adv__brand = document.querySelector('.adv__brand')
+const all = [ welcome__header, ...adv__buttons, socials, adv__brand]
 gsap.set( all, { autoAlpha: 0});
 
 const tl = gsap.timeline({defaults: { ease: 'power3.inOut'}});
@@ -16,4 +19,8 @@ tl.to(".first", {
     duration: .7,
     top: "-100%"
   }, "-=0.5")
-.to(welcome__header, {duration: 1, autoAlpha: 1 })
+.fromTo( welcome__header,{y:"+=100"}, {duration: 1, autoAlpha: 1, y: "-=50" })
+.fromTo( adv__brand, {y:"+=50"}, {duration: .6, autoAlpha: 1, y: "-=50"}, "-=0.6" )
+.fromTo( adv__buttons[0], {x: "+=50"}, {duration: 1, autoAlpha: 1, x:"-=50"}, "-=0.3")
+.fromTo( adv__buttons[1], {x: "+=50"}, {duration: 1, autoAlpha: 1, x:"-=50"}, "-=0.5")
+.fromTo(socials, {x: "-=100"}, { duration: 0.6, autoAlpha: 1, x:"+=100"})
