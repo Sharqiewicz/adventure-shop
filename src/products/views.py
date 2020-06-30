38eddc31ab5094479c3216631083fcbf3fe7ea09
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-06-30 14:36:30 +0200

    add Product context

--- src/products/views.py
+++ src/products/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Product
 # Create your views here.
 
 def homepage_view(request):
-    return render(request, 'index.html', {})
+    products = Product.objects.get(id=2)
+    context = {
+        'title': products.title,
+    }
+    return render(request, 'index.html', context)
